e90a0653b6257d23df59bf69eb861b04a6151882
Harry James Hall <harry@localhost.localdomain>
2026-03-01 14:57:27 +0000

    fix: stabilize local VirtualBox provisioning for bookworm and apt

--- modules/utilities/unix/update/unix_update/manifests/unix.pp
+++ modules/utilities/unix/update/unix_update/manifests/unix.pp
@@ -1,6 +1,28 @@
 class unix_update::unix{
-  case $operatingsystem {
+  case $::operatingsystem {
     'Debian': {
+      # Only force archive.debian.org for legacy Debian releases.
+      # Newer releases (buster/bookworm/...) should keep their default repos.
+      if versioncmp($::operatingsystemrelease, '10') < 0 {
+        file { '/etc/apt/sources.list':
+          ensure  => file,
+          content => "deb http://archive.debian.org/debian stretch main contrib non-free\ndeb http://archive.debian.org/debian-security stretch/updates main contrib non-free\n",
+          before  => Exec['update'],
+        }
+        exec { 'clean-sources':
+          command => "/bin/rm -f /etc/apt/sources.list.d/*.list 2>/dev/null || true",
+          before  => Exec['update'],
+        }
+        file { '/etc/apt/apt.conf.d/99archive':
+          ensure  => file,
+          content => "Acquire::Check-Valid-Until \"false\";\nAcquire::Check-Date \"false\";\nAcquire::AllowInsecureRepositories \"true\";\n",
+          before  => Exec['update'],
+        }
+      } else {
+        file { '/etc/apt/apt.conf.d/99archive':
+          ensure => absent,
+        }
+      }
       exec { 'update':
         command => "/usr/bin/apt-get update --fix-missing",
         tries => 5,
