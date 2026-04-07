314f3ada6b676ed28f1ea566a641614b986d24d2
Harry James Hall <mission.deny.the.mission@gmail.com>
2026-04-07 22:14:25 +0100

    Increase nick length on ngircd server

--- modules/services/unix/irc/ngircd/manifests/config.pp
+++ modules/services/unix/irc/ngircd/manifests/config.pp
@@ -1,4 +1,11 @@
 class ngircd::config {
+  file { '/etc/ngircd/ngircd.conf':
+    ensure  => present,
+    source  => 'puppet:///modules/ngircd/ngircd.conf',
+    require => Package['ngircd'],
+    notify  => Service['ngircd'],
+  }
+
   service { 'ngircd':
     enable   => true,
     ensure   => 'running',
