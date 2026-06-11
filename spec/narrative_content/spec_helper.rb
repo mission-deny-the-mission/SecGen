37f1f22a5d238f8df3755f8985ed16f37098dfc4
Harry James Hall <mission.deny.the.mission@gmail.com>
2026-06-11 06:32:01 +0100

    Fix narrative spec_helper load path for test runnability
    $LOAD_PATH used ../../ which resolves to a non-existent spec/modules/...
    path, so every test died at 'cannot load such file -- llm_provider'.
    Correct to ../../../.

--- spec/narrative_content/spec_helper.rb
+++ spec/narrative_content/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../../modules/generators/narrative_content/lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../../modules/generators/narrative_content/lib', __FILE__)
 
 require 'minitest/autorun'
 require 'json'
